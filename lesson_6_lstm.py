ed003fd516b6e971dac04c45431b4fa0703ca2fe
Sergey Zobin <zobin.sergey@gmail.com>
2019-09-16 10:16:55 +0300

    LSTM-2

--- lesson_6_lstm.py
+++ lesson_6_lstm.py
@@ -1,81 +1,109 @@
-from __future__ import division
-from datetime import datetime, timedelta,date
-import pandas as pd
-# %matplotlib inline
-import matplotlib.pyplot as plt
-import numpy as np
-
 import warnings
-warnings.filterwarnings("ignore")
+warnings.simplefilter(action="ignore", category=Warning)
 
-# import plotly.plotly as py
-# import plotly.offline as pyoff
-# import plotly.graph_objs as go
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # or any {'0', '1', '2'}
 
-#import Keras
-import keras
-from keras.layers import Dense
-from keras.models import Sequential
-from keras.optimizers import Adam 
-from keras.callbacks import EarlyStopping
-from keras.utils import np_utils
-from keras.layers import LSTM
-from sklearn.model_selection import KFold, cross_val_score, train_test_split
+import tensorflow as tf
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.WARN)
 
-#initiate plotly
-# pyoff.init_notebook_mode()
-
-#read the data in csv
-df_sales = pd.read_csv('sales_data.csv')
-
-#convert date field from string to datetime
-df_sales['date'] = pd.to_datetime(df_sales['date'])
-
-#show first 10 rows
-df_sales.head(10)
-
-#create a new dataframe to model the difference
-df_diff = df_sales.copy()
-#add previous sales to the next row
-df_diff['prev_sales'] = df_diff['sales'].shift(1)
-#drop the null values and calculate the difference
-df_diff = df_diff.dropna()
-df_diff['diff'] = (df_diff['sales'] - df_diff['prev_sales'])
-
-#create dataframe for transformation from time series to supervised
-df_supervised = df_diff.drop(['prev_sales'],axis=1)
-#adding lags
-for inc in range(1,13):
-    field_name = 'lag_' + str(inc)
-    df_supervised[field_name] = df_supervised['diff'].shift(inc)
-#drop null values
-df_supervised = df_supervised.dropna().reset_index(drop=True)
-
-#import MinMaxScaler and create a new dataframe for LSTM model
+import requests
+import pandas as pd
+import io
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
-df_model = df_supervised.drop(['sales','date'],axis=1)
-#split train and test set
-train_set, test_set = df_model[0:-6].values, df_model[-6:].values
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dense
+from sklearn.metrics import mean_squared_error
+import math
 
-#apply Min Max Scaler
-scaler = MinMaxScaler(feature_range=(-1, 1))
-scaler = scaler.fit(train_set)
-# reshape training set
-train_set = train_set.reshape(train_set.shape[0], train_set.shape[1])
-train_set_scaled = scaler.transform(train_set)
-# reshape test set
-test_set = test_set.reshape(test_set.shape[0], test_set.shape[1])
-test_set_scaled = scaler.transform(test_set)
 
-X_train, y_train = train_set_scaled[:, 1:], train_set_scaled[:, 0:1]
-X_train = X_train.reshape(X_train.shape[0], 1, X_train.shape[1])
-X_test, y_test = test_set_scaled[:, 1:], test_set_scaled[:, 0:1]
-X_test = X_test.reshape(X_test.shape[0], 1, X_test.shape[1])
+def get_silso():
+    r = requests.get("http://www.sidc.be/silso/INFO/snmtotcsv.php")
+    if r.status_code != 200:
+        print('get data error {}'.format(r.status_code))
+        return -1
+    csv = "year;month;time;activity;o1;o2;o3\n" + r.content.decode("utf-8")  
+    df = pd.read_csv(io.StringIO(csv), delimiter=';')
+    df1 = df[["time", "activity"]]
+    return df1
 
-model = Sequential()
-model.add(LSTM(4, batch_input_shape=(1, X_train.shape[1], X_train.shape[2]), stateful=True))
-model.add(Dense(1))
-model.compile(loss='mean_squared_error', optimizer='adam')
-model.fit(X_train, y_train, nb_epoch=100, batch_size=1, verbose=1, shuffle=False)
 
-y_pred = model.predict(X_test,batch_size=1)
+def create_dataset(data_set, lookback):
+    data_x, data_y = [], []
+    for i in range(len(data_set)-lookback-1):
+        a = data_set[i:(i+lookback)]
+        data_x.append(a)
+        data_y.append(data_set[i+lookback])
+    return np.array(data_x), np.array(data_y)
+
+
+def main():
+    df1 = get_silso()
+    yy = df1['activity'].values
+    # x = df1['time'].values
+
+    # нормализация данных
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    ym = scaler.fit_transform(yy.reshape((-1, 1)))
+
+    #разбиваем на training and testing set
+    train_size = int(len(ym)*2/3)
+    # test_size = len(ym)-train_size
+
+    train, test = ym[0:train_size], ym[train_size+1:]
+
+    lookback = 24
+    n_features = 1
+    train_x, train_y = create_dataset(train, lookback)
+    test_x, test_y = create_dataset(test, lookback)
+
+    train_x = np.reshape(train_x, (train_x.shape[0], train_x.shape[1], n_features))
+    test_x = np.reshape(test_x, (test_x.shape[0], test_x.shape[1], n_features))
+
+    # Создание модели LSTM
+
+    model = Sequential()
+    model.add(LSTM(4, input_shape=(lookback, n_features)))
+    model.add(Dense(1))
+    model.compile(loss="mean_squared_error", optimizer='adam')
+
+    model.fit(train_x, train_y, epochs=2, batch_size=1, verbose=2)
+
+    train_predict = model.predict(train_x)
+    test_predict = model.predict(test_x)
+    train_predict = scaler.inverse_transform(train_predict)
+    test_predict = scaler.inverse_transform(test_predict)
+
+    print(train_y)
+    print(train_predict[:, 0])
+    # Статистика модели
+    train_score = math.sqrt(mean_squared_error(train_y[:, 0], train_predict[:, 0]))
+    test_score = math.sqrt(mean_squared_error(test_y[:, 0], test_predict[:, 0]))
+    print("Train score ={0:.3f}".format(train_score))
+    print("Test score ={0:.3f}".format(test_score))
+
+    # рисуем
+
+    train_predict_plot = np.empty_like(ym)
+    train_predict_plot[:, :] = np.nan
+    train_predict_plot[lookback:len(train_predict) + lookback, :] = train_predict
+
+    print(train_predict_plot)
+
+    test_predict_plot = np.empty_like(ym)
+    test_predict_plot[:, :] = np.nan
+    test_predict_plot[len(train_predict) + lookback * 2+2:len(ym) - 1, :] = test_predict
+
+    print(test_predict_plot)
+
+    plt.plot(scaler.inverse_transform(ym))
+    plt.plot(train_predict_plot)
+    plt.plot(test_predict_plot)
+    # plt.show()
+    plt.savefig("lstm.png")
+
+
+main()
